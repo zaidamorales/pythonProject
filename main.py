da56e9e1acd0b7c362d0305914b3dc5cd119ac17
zaidamorales <zmorales@institutmvm.cat>
2021-12-02 13:16:36 +0100

    Initial commit

--- main.py
+++ main.py
@@ -4,16 +4,16 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 def main():
-   aux = aux2 = 0
-   num = int(input("Introdueix un nombre enter: "))
-   while num < 1:
-      num = int(input("Introdueix un nombre enter: "))
-   for x in range(num):
-      aux += x
-      if aux < num:
-         print(x, end= " ")
-         aux2 += x
-   print("\nLa suma dels valors és: ", aux2)
+   nom = input("Introduce el nombre: ")
+   cognom = input("Introduce el pimer apellido: ")
+   segundo = input("Introduce el segundo apellido: ")
+
+
+   print ("*********************")
+
+   print(cognom[:2], end="")
+   print(segundo[:2], end="")
+   print(nom[:2], end="")
 
 if __name__ == '__main__':
    main()
