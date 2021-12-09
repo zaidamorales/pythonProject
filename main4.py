008508799f8bf9973e9e430714961565073d6f86
zaidamorales <zmorales@institutmvm.cat>
2021-12-09 13:18:12 +0100

    A partir de la següent frase: state = “Practica els problemes de list comprehensions per a ser més Pythonic!” conta els espais que conté i indica-ho per pantalla.

--- main4.py
+++ main4.py
@@ -3,7 +3,7 @@ def main():
   state = "Practica els problemes de list comprehensions per a ser més Pythonic!"
   espacio = [i for i in state if i in ' ']
 
-  print(espacio)
+  print(len(espacio))
 
 if __name__ == '__main__':
   main()
