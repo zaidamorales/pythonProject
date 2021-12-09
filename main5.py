b1ff5acc75cf28c074e0ab39c9e1277ea457971a
zaidamorales <zmorales@institutmvm.cat>
2021-12-09 13:23:55 +0100

    Elimina totes les vocals de la cadena anterior i mostra el resultat per pantalla.

--- main5.py
+++ main5.py
@@ -1,9 +1,9 @@
 def main():
 
    state = "Practica els problemes de list comprehensions per a ser més Pythonic!"
-   vowels = [i for i in state if i in 'BCDFGHJKLMNPQRSTVWXZbcdfghjklmnpqrstvwxz']
+   vowels = [i for i in state if i in 'Pbcdfghjklmnpqrstvwxyz']
 
-   print(lent(vowels))
+   print(vowels)
 
 if __name__ == '__main__':
    main()
