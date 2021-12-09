d72b314f534bb2b79d51bb7bd6fe5ed4957bb002
zaidamorales <zmorales@institutmvm.cat>
2021-12-09 12:25:50 +0100

    Initial commit

--- main1.py
+++ main1.py
@@ -1,13 +1,7 @@
 def main():
-   n1 = int(input("Introduce el primer numero: "))
-   n2 = int(input("Introduce el segundo numero: "))
+    llista = [x * x for x in range(10)]
 
-   if n1 > n2:
-      print("Error")
-   else:
-     for n1 in range (n1, n2):
-        print(n1)
-        n1 += 1
+    print (llista)
 
 if __name__ == '__main__':
-  main()
+   main()
