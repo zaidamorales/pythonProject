6393f0775d9424fd6521c82277f4d701e7b3f078
zaidamorales <zmorales@institutmvm.cat>
2021-12-10 12:09:39 +0100

    Dissenya un algorisme que mostri per pantalla tots els nombres entre 1 i 1000 que tenen un 6 (inclosos).

--- main3.py
+++ main3.py
@@ -1,9 +1,8 @@
 def main():
 
-    llista = [x for x in range(0,1001)]
-    llista_dos = [x for x in llista if str(x) in '6, 16, 26']
+    llista = [x for x in range(1001) if '6' in str(x)]
 
-    print(llista_dos)
+    print(llista)
 
 if __name__ == '__main__':
   main()
